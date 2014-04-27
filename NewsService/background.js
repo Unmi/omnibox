8e04655d040eab2e0e033106364012dced2f9e6a
Unmi Qiu <unmi.qiu@morningstar.com>
2014-04-27 14:13:01 -0500

    support symbol, template parameters

--- NewsService/background.js
+++ NewsService/background.js
@@ -3,19 +3,39 @@ chrome.omnibox.onInputChanged.addListener(
     console.log('inputChanged: ' + text);
     suggest([
       {content: text + "id=", description: "show news content by id"},
-      {content: text + "symbol=", description: "search news by symbole"}
+      {content: text + "symbol=", description: "search news by symbol"}
     ]);
   });
 
-chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-    console.log('inputEntered: ' + text);
-	if(text.indexOf("id=")==0){
-		var newsId = text.substring(3);
-		var newsContentUrl = "http://newsservice.morningstar.com/news/services/newsRS/view?productcode=RMOB&resourceId="+newsId;
-		//window.location = newsContentUrl;
-		chrome.tabs.getSelected(null, function(tab){
-			chrome.tabs.update(tab.id, {url: newsContentUrl});
-		});
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  var params = [];
+  var paramsInArray = text.split(" ");
+
+  for(i=0; i< paramsInArray.length; i++){
+    var keyValue = paramsInArray[i].split("=");
+    if(keyValue.length >= 2){
+      params[keyValue[0]] = keyValue[1];
+    }
+  }
+
+  var baseNewsServiceUrl = "http://newsservice.morningstar.com/news/services/newsRS";
+	if(params["id"] !== undefined){
+		var newsId = params["id"];
+		var newsContentUrl = baseNewsServiceUrl +"/view?productcode=RMOB&resourceId="+newsId;
+		openUrlInCurrentTab(newsContentUrl);
+	}else if(params["symbol"] !== undefined){
+		var symbol = params["symbol"];
+    var type = symbol.indexOf(":") == -1 ? "SC" : "CS";
+		var newsUrl = baseNewsServiceUrl + "/list?productCode=RMOB&idType="+type+"&symbol="+symbol;
+    if(params["template"] !== undefined){
+      newsUrl += "&template=" + params["template"];
+    }
+		openUrlInCurrentTab(newsUrl);
 	}
-  });
+});
+
+function openUrlInCurrentTab(newUrl){
+	chrome.tabs.getSelected(null, function(tab){
+		chrome.tabs.update(tab.id, {url: newUrl});
+	});
+}
