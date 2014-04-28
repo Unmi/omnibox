06e78d21f8d8feddd839f66ed6993f1858e0286c
Unmi Qiu <unmi.qiu@morningstar.com>
2014-04-28 16:58:17 -0500

    fixed the comma punctuation

--- SecurityService/background.js
+++ SecurityService/background.js
@@ -5,17 +5,14 @@ chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     suggest([
       {content: "sample1", description: "securityId/view, Info, Quote, Details, News, DailyPriceVolume, IntradayPriceVolume, filter="},
-      {content: "sample2", description: "securityId/News/[Generic, Research, Commentary, Video], analyst_reports, analyst_reports/latest"}，
-      {content: "sample3", description: "securityIds/top_news, viewall, securityId/search/{key}"}
+      {content: "sample2", description: "securityId/News/[Generic, Research, Commentary, Video], analyst_reports, analyst_reports/latest"},
+      {content: "sample3", description: "securityIds/top_news, viewall, securityId/search/key"}
     ]);
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
   text = text.trim();
-  var url = "https://mobileservice.morningstar.com/service/1.0/locales/en-US/products/RT/securities/";
-
-  url += text;
-	
+  var url = "https://mobileservice.morningstar.com/service/1.0/locales/en-US/products/RT/securities/" + text;
   openUrlInCurrentTab(url);
 
 });
